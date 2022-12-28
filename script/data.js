00d1e9e0461b72401d616e62736d87db4b42740d
Elton Hoffmann <elton.hoffmann@mercadolivre.com>
2022-12-28 17:14:09 -0300

    Add dummy data

---
+++ script/data.js
@@ -0,0 +1,43 @@
+const Data = [{
+	rows: [{
+		title: "#examle",
+		items: [
+		{
+			"name": "example",
+			"href": "https://example.com/"
+		}
+	]
+	},{
+		title: "#examle",
+		items: [
+		{
+			"name": "example",
+			"href": "https://example.com/"
+		}
+	]
+	}]
+}, {
+	rows: [{
+		title: "#examle",
+		items: [
+		{
+			"name": "example",
+			"href": "https://example.com/",
+			"tags": [{
+				"name": "git",
+				"href": "http://github.com/example"
+			}]
+		}
+	]
+	}]
+}, {
+	rows: [{
+		title: "#examle",
+		items: [
+		{
+			"name": "example",
+			"href": "https://example.com/"
+		}
+	]
+	}]
+}]
