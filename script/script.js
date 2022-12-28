2b37da499bb1d3463dfa458b6c54a2e89039393a
Elton Hoffmann <elton.hoffmann@mercadolivre.com>
2022-12-28 12:25:10 -0300

    Move data to a separate script

--- script/script.js
+++ script/script.js
@@ -1,49 +1,5 @@
 'use strict';
 
-const Data = [{
-	rows: [{
-		title: "#examle",
-		items: [
-		{
-			"name": "example",
-			"href": "https://example.com/"
-		}
-	]
-	},{
-		title: "#examle",
-		items: [
-		{
-			"name": "example",
-			"href": "https://example.com/"
-		}
-	]
-	}]
-}, {
-	rows: [{
-		title: "#examle",
-		items: [
-		{
-			"name": "example",
-			"href": "https://example.com/",
-			"tags": [{
-				"name": "git",
-				"href": "http://github.com/example"
-			}]
-		}
-	]
-	}]
-}, {
-	rows: [{
-		title: "#examle",
-		items: [
-		{
-			"name": "example",
-			"href": "https://example.com/"
-		}
-	]
-	}]
-}]
-
 const Main = (() => {
 	const init = () => {
 		const contentArea = document.getElementById("content");
