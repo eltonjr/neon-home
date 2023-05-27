46be5c3b538022e77b81e8404787480ea1313a10
Elton Hoffmann <elton.hoffmann@mercadolivre.com>
2023-05-27 13:44:59 -0300

    Redirect feature + definitive name

--- script/script.js
+++ script/script.js
@@ -4,16 +4,24 @@ const Main = (() => {
 	const init = () => {
 		const contentArea = document.getElementById("content");
 		contentArea.innerHTML = Data.map(col => `<div class="column">
-			${col.rows.map(({title, items}) => `
-				<h3>${title}</h3>
-				<ul class="row">
-				${items.map(({name, href, tags}) => `<li>
-                    <a href="${href}">${name}</a>&nbsp&nbsp${(tags || []).map(({name, href}) => `
-                    [<a class="${name}" href="${href}">${name}</a>]`).join(" ")}
-				</li>`).join("")}
-			</ul>`).join("")}
+			${(col.rows || []).map(({title, items, href}) =>
+				href
+				? onlyLink(href, title)
+				: blockWithList(title, items))
+			.join("")}
 		</div>`).join("");
 	}
+
+	const onlyLink = (href, title) => `<h3><a class="redirect" href="${href}">${title}</a></h3>`;
+	const blockWithList = (title, items) => {
+		return `<h3>${title}</h3>
+				<ul class="row">
+					${(items || []).map(({name, href, tags}) => `<li>
+						<a href="${href}">${name}</a>&nbsp&nbsp${(tags || []).map(({name, href}) => `
+						[<a class="${name}" href="${href}">${name}</a>]`).join(" ")}
+					</li>`).join("")}
+				</ul>`;
+	};
 
 	return {
 		init
